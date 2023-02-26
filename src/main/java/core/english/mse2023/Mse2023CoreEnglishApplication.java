b6f0e5c98a3425110a90dc84e27ae802379764dc
Pavel Tabakov <ptabakov@intetech.ru>
2023-02-26 22:29:06 +0300

    initial

---
+++ src/main/java/core/english/mse2023/Mse2023CoreEnglishApplication.java
@@ -0,0 +1,13 @@
+package core.english.mse2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mse2023CoreEnglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mse2023CoreEnglishApplication.class, args);
+    }
+
+}

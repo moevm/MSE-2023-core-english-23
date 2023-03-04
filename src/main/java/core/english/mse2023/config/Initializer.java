19e1514bf1a100df70086ded0d2c25a18aa07f1c
detkovaannya <detkovaannya@gmail.com>
2023-03-04 11:43:28 +0300

    done: бот подключен к проекту

---
+++ src/main/java/core/english/mse2023/config/Initializer.java
@@ -0,0 +1,29 @@
+package core.english.mse2023.config;
+
+import core.english.mse2023.tgbot.CounterTelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+public class Initializer {
+    @Autowired
+    CounterTelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot((LongPollingBot) bot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}

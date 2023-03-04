151b25606860173e54af29bf1b3462574925848c
Detkova Anna <71722131+detkovaannya@users.noreply.github.com>
2023-03-04 17:03:51 +0300

    done: бот подключен к проекту (#24)
    * done: бот подключен к проекту
    
    * update: исправлен кодстайл
    
    * Update BotConfig.java

---
+++ src/main/java/core/english/mse2023/config/Initializer.java
@@ -0,0 +1,30 @@
+package core.english.mse2023.config;
+
+import core.english.mse2023.tgbot.CounterTelegramBot;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Initializer {
+    private final CounterTelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot((LongPollingBot) bot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}

1b0a8f878ed39d74d1b8d1eabe91ad1f8caa0daa
Pavel <53811013+PashaThrowsException78@users.noreply.github.com>
2023-03-17 12:55:29 +0300

    add InlineKeyboardBuilder (#66)
    * add InlineKeyboardBuilder
    
    * little changes
    
    ---------
    
    Co-authored-by: Pavel Tabakov <ptabakov@intetech.ru>

---
+++ src/main/java/core/english/mse2023/util/builder/InlineKeyboardBuilder.java
@@ -0,0 +1,47 @@
+package core.english.mse2023.util.builder;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class InlineKeyboardBuilder {
+
+    private final List<List<InlineKeyboardButton>> buttons;
+    private int buildableRow = -1;
+
+    private InlineKeyboardBuilder() {
+        buttons = new ArrayList<>();
+    }
+
+    public static InlineKeyboardBuilder instance() {
+        return new InlineKeyboardBuilder();
+    }
+
+    public InlineKeyboardBuilder row() {
+        buttons.add(new ArrayList<>());
+        buildableRow++;
+
+        return this;
+    }
+
+    public InlineKeyboardBuilder button(String text) {
+        return button(new InlineKeyboardButton(text));
+    }
+
+    public InlineKeyboardBuilder button(InlineKeyboardButton button) {
+        if (buttons.isEmpty()) {
+            row();
+        }
+
+        buttons.get(buildableRow).add(button);
+
+        return this;
+    }
+
+    public InlineKeyboardMarkup build() {
+        return new InlineKeyboardMarkup(buttons);
+    }
+
+}

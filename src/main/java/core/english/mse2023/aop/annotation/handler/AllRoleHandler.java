23c2c295b0e3c3bc1dae89fcaec2cd90b3d3dc16
Ilya <ilya.exsaun@gmail.com>
2023-03-18 22:24:21 +0300

    Added retention to AllRoleHandler

--- src/main/java/core/english/mse2023/aop/annotation/handler/AllRoleHandler.java
+++ src/main/java/core/english/mse2023/aop/annotation/handler/AllRoleHandler.java
@@ -1,8 +1,12 @@
 package core.english.mse2023.aop.annotation.handler;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @AdminHandler
 @ParentHandler
 @StudentHandler
 @TeacherHandler
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AllRoleHandler {
 }
